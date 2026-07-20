8cf554ca799f7ebd288047e21a918f4ab0a4a2f4
Dennis Moe <dennismoe0@gmail.com>
2026-07-20 13:39:49 +0200

    Basic parser, not fine tuned at all + no edge cases whatsoever

---
+++ src/main/java/no/moefrilans/regnskap/CsvParser.java
@@ -0,0 +1,32 @@
+package no.moefrilans.regnskap;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.Buffer;
+
+public class CsvParser {
+
+    public static void main(String[] args) {
+        // Should be changed so input decides path
+        String filePath = "eksempel.csv";
+        String line;
+        String delimiter = ";";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            // Stops if empty line, probably needs to handle edge cases (sudden empty line)
+            while((line = br.readLine()) != null) {
+                String[] values = line.split(delimiter); // Splits by delimiter
+                // print for visual testing without test class atm.
+                for (String value : values) {
+                    System.out.println(value + " ");
+                }
+                System.out.println();
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
+
