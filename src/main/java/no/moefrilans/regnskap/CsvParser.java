1c80ea8fc115bb5630f7cac48456a9db283381d1
Dennis Moe <dennismoe0@gmail.com>
2026-07-20 13:40:04 +0200

    Basic parser, not fine tuned at all + no edge cases whatsoever

--- src/main/java/no/moefrilans/regnskap/CsvParser.java
+++ src/main/java/no/moefrilans/regnskap/CsvParser.java
@@ -1,10 +1,8 @@
 package no.moefrilans.regnskap;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.Buffer;
 
 public class CsvParser {
 
