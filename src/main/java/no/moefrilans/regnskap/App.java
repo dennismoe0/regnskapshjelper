83bcbc513d8584e03f491dc3e0aef449332618f4
Dennis Moe <dennismoe0@gmail.com>
2026-07-22 00:02:42 +0200

    chore: commiting random changes, inconsequential

---
+++ src/main/java/no/moefrilans/regnskap/App.java
@@ -0,0 +1,13 @@
+package no.moefrilans.regnskap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+  public static void main(String[] args) {
+    Transaction testingTransaction1 = new Transaction("20.03.2026", "Testing Name", "B",
+        "Overføring", "20.03.2026", "", "150,00", "6767", "420 test");
+  }
+}
